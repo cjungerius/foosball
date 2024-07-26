b1c4c68a90e8587d596b770d6513bd49ec9b09b6
Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
2024-07-26 16:52:12 +0200

    update scores and rerun models

--- foosball_ratings.R
+++ foosball_ratings.R
@@ -86,7 +86,7 @@ post_pred %>%
   theme(text=element_text(size=20,family="Roboto"))
 
 #pure player coefficients (probably more useful?)
-gather_draws(m1,`b_[a-zš]+`,regex=T) %>% 
+p2 <- gather_draws(m1,`b_[a-zš]+`,regex=T) %>% 
   separate_wider_delim(.variable,'_',names = c("b","name")) %>% 
   mutate(name = str_to_title(name)) %>% 
   ggplot(aes(
